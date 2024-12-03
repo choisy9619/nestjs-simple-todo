7e20bbf3b1ba97d860cc74795afb1a5006cb1f9e
seoyoung <choisy9619@gmail.com>
2024-12-03 10:40:34 +0900

    feat: API response - decorator

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,4 +1,19 @@
+import { IsString, IsBoolean, MinLength, MaxLength } from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString({
+    message: '제목은 문자열만 가능합니다',
+  })
+  @MinLength(6, {
+    message: '제목은 $constraint1자 이상 작성해주세요 (입력 값: $value)',
+  })
+  @MaxLength(30, {
+    message: '제목은 $constraint1자를 넘길 수 없습니다 (입력 값: $value)',
+  })
   title: string;
+
+  @IsBoolean({
+    message: 'Boolean 타입만 가능합니다',
+  })
   is_done: boolean;
 }
--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  ParseIntPipe,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
@@ -9,26 +19,31 @@ export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
   @Post()
+  @HttpCode(HttpStatus.OK)
   create(@Body() createTodoDto: CreateTodoDto) {
     return this.todosService.create(createTodoDto);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.todosService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.todosService.findOne(+id);
   }
 
   @Post(':id')
+  @HttpCode(HttpStatus.OK)
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todosService.update(+id, updateTodoDto);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.todosService.remove(+id);
   }
